eeee813ccf205c62e558af8857d99e1ca26c8f6e
gshaymond <gshaymond@byui.edu>
2025-03-27 14:00:23 -0600

    removed unused code

--- src/lib/components/CardList.svelte
+++ src/lib/components/CardList.svelte
@@ -1,22 +1,14 @@
 <script>
   import RecipeCard from "./RecipeCard.svelte";
-  import { getRandomRecipes } from "../external-services.mjs";
 
   let { recipes } = $props();
 
-  // call getRandomRecipes function
-  // let recipes = await getRandomRecipes();
 </script>
 
 <section class="card-list">
   {#each recipes as recipe}
     <RecipeCard {recipe} />
   {/each}
-
-  <!-- displaying random recipies -->
-  <!-- {#each recipes.slice(0, 14) as recipe}
-        <RecipeCard recipe={recipe}/>
-    {/each} -->
 </section>
 
 <style>
